3d317b31dbfa89d1611b24220262c0c81e8fc464
Bhanu Lamichhane <bhanu.lamichhane89@gmail.com>
2020-12-29 16:05:18 +0545

    remainder

--- calculator.c
+++ calculator.c
@@ -2,17 +2,22 @@
 
 int diffNum(int , int);
 int sum(int,int);
+int remain(int , int);
+
 int  main(){
     int a;
     int b;
     int subtract;
+    int rem;
     printf("\nEnter any number :");
     scanf("%d",&a);
     printf("\nEnter another number :");
     scanf("%d",&b);
     subtract=diffNum(a,b);
-printf("The subtraction of these two number is :%d",subtract);
-printf("\nThe sum of %d and %d is %d",a,b,sum(a,b));
+printf("The subtraction of these two number is :%d \n",subtract);
+printf("\nThe sum of %d and %d is %d \n",a,b,sum(a,b));
+rem=remain(a,b);
+printf("The remainder of these two number is :%d",rem);
     return 0;
 }
 int diffNum(int a, int b)
@@ -27,4 +32,9 @@ int sum(int x, int y)
 {
     return x+y;
 
+}
+int remain(int a, int b)
+
+{
+return a%b;
 }
