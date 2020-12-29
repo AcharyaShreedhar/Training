b69dc5072e8ddd017391b2e31e7b3656a769604a
Ankit Pangeni <ankitpangeni74@gmail.com>
2020-12-29 14:49:09 +0545

    Division

--- calculator.c
+++ calculator.c
@@ -3,10 +3,23 @@
 int  main(){
     int a;
     int b;
+    int div(int a , int b);
+
     printf("\nEnter any number");
     scanf("%d",&a);
     printf("\nEnter another number");
     scanf("%d",&b);
+    division(a,b);
     
     return 0;
+
 }
+
+int division(int a,int b)
+{
+int div;
+div=a/b;
+printf("\n The result is: %d",div);
+getch();
+}
+
